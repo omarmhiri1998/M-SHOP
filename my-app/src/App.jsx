9f88ac46950f9f20c6b9d0a789bf2a28608004f3
Omar Mhiri <omar.mhiri998@gmail.com>
2026-08-10 22:08:08 +0200

    first commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
-
+import "./App.css";
 function App() {
   return (
     <div className="app">
