d456a85f34e2757985696c597c5698602c050e0d
Omar Mhiri <omar.mhiri998@gmail.com>
2026-08-10 22:05:47 +0200

    create react project

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      Navbar
+    </nav>
+  );
+}
+
+export default Navbar;
